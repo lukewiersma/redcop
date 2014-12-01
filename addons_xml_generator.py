ca6277db8025711111bdd9764c25acec87e72914
lukewiersma <lukewiersma@users.noreply.github.com>
2014-12-01 16:08:10 -0600

    Update addons_xml_generator.py

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -17,7 +17,7 @@ class Generator:
         # addon list
         addons = os.listdir( "." )
         # final addons text
-        addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
+        addons_xml = u"<?xml version=\"2.1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
         # loop thru and add each addons addon.xml file
         for addon in addons:
             try:
